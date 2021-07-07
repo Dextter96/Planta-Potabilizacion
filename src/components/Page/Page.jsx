6e675ece7aab15f2e8eaf8fe451a8c4637c601b9
Sebastian Miranda <arqcompu4@gmail.com>
2021-07-07 17:46:27 -0500

    general config and styles in page component

--- src/components/Page/Page.jsx
+++ src/components/Page/Page.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import './Page.scss'
 
 const Page = React.forwardRef((props, ref) => {
-    const {content} = props;
+    const {content, title, number} = props;
 
-    const showText = () => {
+    const showContent = () => {
         if(content) {
             const values = Object.values(content);
             const keys = Object.keys(content);
@@ -13,18 +13,18 @@ const Page = React.forwardRef((props, ref) => {
                 let element;
                 switch(keys[i].split('_')[0]) {
                     case 'subtitle':
-                        element = <h3 key={values[i]}>{values[i]}</h3>
+                        element = <h3 key={i}>{values[i]}</h3>
                         break;
                     case 'image':
-                        element = <img key={values[i]} src={`/assets/images/${values[i]}`} />
+                        element = <img key={i} src={`/assets/images/${values[i]}`} alt=""/>
                     break;
                     case 'text':
-                        element = <p key={values[i]} >{values[i]}</p>
+                        element = <p key={i} >{values[i]}</p>
                     break;
                     case 'list':
-                        element = <ul key={values[i]}>
-                            {values[i].map((page,j) =>{ return <li>{values[i][j]}</li>})}
-                            </ul>
+                        element = <ul key={i}>
+                            {values[i].map((page,j) =>{ return <li key={j}>{values[i][j]}</li>})}
+                        </ul>
                     break;
                     default:
                         element = <></>
@@ -35,14 +35,15 @@ const Page = React.forwardRef((props, ref) => {
 
         return <></>;
     }
+
     return(
         <div className="page" ref={ref}>
             <div className="page_content">
-                <h2 className="page_title">{props.title}</h2>
-                <div className="page_text">
-                    {showText()}
+                <h2 className="page_title">{title}</h2>
+                <div className="page_content">
+                    {showContent()}
                 </div>
-                <div className="page_footer">{props.number}</div>
+                <div className="page_footer">{number}</div>
             </div>
         </div>
     );
